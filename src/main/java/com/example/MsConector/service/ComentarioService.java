1b4c4f49762879dada5f1ee00bfd2077e3fbda5e
Tecno321 <152120786+Tecno321@users.noreply.github.com>
2025-05-22 16:46:05 +0000

    cambio de puertos y correcion de url

--- src/main/java/com/example/MsConector/service/ComentarioService.java
+++ src/main/java/com/example/MsConector/service/ComentarioService.java
@@ -10,10 +10,10 @@ public class ComentarioService {
     private RestTemplate restTemplate;
 
     public String obtenerMateriasPorProfesor(int id){
-        String usuarioUrl = "http://localhost:8080/profesores/obtenerProfe/"+id;
+        String usuarioUrl = "http://localhost:8082/profesores/obtenerProfe/"+id;
         String usuarioData = restTemplate.getForObject(usuarioUrl, String.class);
         
-        String materiaUrl = "http://localhost:8082/obtenerMaterias/"+id;
+        String materiaUrl = "http://localhost:8080/api/materias/"+id;
         String materiaData = restTemplate.getForObject(materiaUrl, String.class);
 
         return "{\"usuario\": "+usuarioData + " ,\"materias\": "+ materiaData+" }";
